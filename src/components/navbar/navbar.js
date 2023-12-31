4c5e76db90350186bf4c1d754cc84fbc258355c6
Rodrigo Valdivieso <vrodrigoaaron@gmail.com>
2023-12-30 23:55:43 -0300

    empezando react

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <nav>
+
+            <h3>mitennis</h3>
+            <div>
+                <button>Raquetas</button>
+                <button>Pelotas</button>
+                <button>Cuerdas</button>
+                <button>Indumentaria</button>
+            </div>
+        </nav>
+    )
+
+
+
+}
+
+export default Navbar
